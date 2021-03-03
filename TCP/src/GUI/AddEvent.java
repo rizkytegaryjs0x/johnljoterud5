046a122ead48fab05fdf8ea9f2c52b8a234e2f23
rizkytegaryjs0x <wjx1234567@yeah.net>
2021-03-03 08:07:50 +0800

    AddEvent & Note GUI

---
+++ TCP/src/GUI/AddEvent.java
@@ -0,0 +1,349 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+
+	public class AddEvent extends JPanel {
+		private JTextField textField_Location;
+		private JTextField textFieldStartYear;
+		private JButton btnSubmit;
+		private JButton btnBack;
+		private JLabel lblCBSlogo;
+		private JLabel lblCreate;
+		private JLabel lblEnd;
+		private JLabel lblName;
+		private JLabel lblText;
+		private JLabel lblEmail;
+		private JLabel lblBackground;
+		private JLabel lblCreateddate;
+		private JTextField textField_Name;
+		private JTextField textField_Text;
+		private JLabel lblType;
+		private JTextField textField_Type;
+		private JTextField textFieldStartMonth;
+		private JTextField textFieldStartHour;
+		private JTextField textFieldStartMinute;
+		private JTextField textFieldStartSecond;
+		private JTextField textFieldEndSecond;
+		private JTextField textFieldEndMinute;
+		private JTextField textFieldEndHour;
+		private JTextField textFieldEndMonth;
+		private JTextField textFieldEndYear;
+		private JTextField textFieldStartDateDay;
+		private JTextField textFieldEndDateDay;
+		private JLabel lblStartDate;
+		private JLabel lblHour;
+		private JLabel lblNewLabel_1;
+		private JLabel lblSecond;
+
+		//Tilføjer et panel der kan tilføje events. Heri oprettes alle JLabels, JButtons og JTextfields.
+		public AddEvent() {
+			setPreferredSize(new Dimension(1366, 768));
+			setSize(new Dimension(1366, 768));
+			setLayout(null);
+			
+			textFieldEndDateDay = new JTextField();
+			textFieldEndDateDay.setForeground(SystemColor.controlDkShadow);
+			textFieldEndDateDay.setColumns(10);
+			textFieldEndDateDay.setBounds(885, 346, 53, 34);
+			add(textFieldEndDateDay);
+			
+			textFieldStartDateDay = new JTextField();
+			textFieldStartDateDay.setForeground(SystemColor.controlDkShadow);
+			textFieldStartDateDay.setColumns(10);
+			textFieldStartDateDay.setBounds(885, 297, 53, 34);
+			add(textFieldStartDateDay);
+
+			textFieldEndSecond = new JTextField();
+			textFieldEndSecond.setForeground(SystemColor.controlDkShadow);
+			textFieldEndSecond.setColumns(10);
+			textFieldEndSecond.setBounds(1080, 346, 53, 34);
+			add(textFieldEndSecond);
+
+			textFieldEndMonth = new JTextField();
+			textFieldEndMonth.setForeground(SystemColor.controlDkShadow);
+			textFieldEndMonth.setColumns(10);
+			textFieldEndMonth.setBounds(820, 346, 53, 34);
+			add(textFieldEndMonth);
+
+			textFieldEndMinute = new JTextField();
+			textFieldEndMinute.setForeground(SystemColor.controlDkShadow);
+			textFieldEndMinute.setColumns(10);
+			textFieldEndMinute.setBounds(1015, 346, 53, 34);
+			add(textFieldEndMinute);
+
+			textFieldEndHour = new JTextField();
+			textFieldEndHour.setForeground(SystemColor.controlDkShadow);
+			textFieldEndHour.setColumns(10);
+			textFieldEndHour.setBounds(950, 346, 53, 34);
+			add(textFieldEndHour);
+
+			textFieldEndYear = new JTextField();
+			textFieldEndYear.setForeground(SystemColor.controlDkShadow);
+			textFieldEndYear.setColumns(10);
+			textFieldEndYear.setBounds(755, 346, 53, 34);
+			add(textFieldEndYear);
+
+			textFieldStartSecond = new JTextField();
+			textFieldStartSecond.setForeground(SystemColor.controlDkShadow);
+			textFieldStartSecond.setColumns(10);
+			textFieldStartSecond.setBounds(1080, 297, 53, 34);
+			add(textFieldStartSecond);
+
+			textFieldStartMinute = new JTextField();
+			textFieldStartMinute.setForeground(SystemColor.controlDkShadow);
+			textFieldStartMinute.setColumns(10);
+			textFieldStartMinute.setBounds(1015, 297, 53, 34);
+			add(textFieldStartMinute);
+
+			textFieldStartHour = new JTextField();
+			textFieldStartHour.setForeground(SystemColor.controlDkShadow);
+			textFieldStartHour.setColumns(10);
+			textFieldStartHour.setBounds(950, 297, 53, 34);
+			add(textFieldStartHour);
+
+			textFieldStartMonth = new JTextField();
+			textFieldStartMonth.setForeground(SystemColor.controlDkShadow);
+			textFieldStartMonth.setColumns(10);
+			textFieldStartMonth.setBounds(820, 297, 53, 34);
+			add(textFieldStartMonth);
+			
+			btnBack = new JButton("Back");
+			btnBack.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btnBack.setBorder(new CompoundBorder(new BevelBorder(
+					BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0,
+							0), new Color(255, 255, 255), new Color(0, 0, 0)),
+					new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255),
+							new Color(0, 0, 0), new Color(255, 255, 255),
+							new Color(0, 0, 0))));
+			btnBack.setForeground(new Color(255, 255, 255));
+			btnBack.setFont(new Font("Arial", Font.BOLD, 30));
+			btnBack.setContentAreaFilled(false);
+			btnBack.setBounds(552, 650, 194, 50);
+			add(btnBack);
+
+			textField_Location = new JTextField();
+			textField_Location.setForeground(new Color(105, 105, 105));
+			textField_Location.setColumns(10);
+			textField_Location.setBounds(753, 231, 120, 34);
+			add(textField_Location);
+
+			lblEmail = new JLabel("Location");
+			lblEmail.setForeground(new Color(255, 255, 255));
+			lblEmail.setFont(new Font("Arial", Font.BOLD, 26));
+			lblEmail.setBounds(483, 235, 109, 30);
+			add(lblEmail);
+
+			textFieldStartYear = new JTextField();
+			textFieldStartYear.setForeground(new Color(105, 105, 105));
+			textFieldStartYear.setColumns(10);
+			textFieldStartYear.setBounds(755, 297, 53, 34);
+			add(textFieldStartYear);
+
+			lblCreateddate = new JLabel("Start");
+			lblCreateddate.setForeground(new Color(255, 255, 255));
+			lblCreateddate.setFont(new Font("Arial", Font.BOLD, 26));
+			lblCreateddate.setBounds(483, 300, 159, 31);
+			add(lblCreateddate);
+
+			btnSubmit = new JButton("Create");
+			btnSubmit.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+			btnSubmit.setContentAreaFilled(false);
+			btnSubmit.setBorder(new CompoundBorder(new BevelBorder(
+					BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0,
+							0), new Color(255, 255, 255), new Color(0, 0, 0)),
+					new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255),
+							new Color(0, 0, 0), new Color(255, 255, 255),
+							new Color(0, 0, 0))));
+			btnSubmit.setForeground(new Color(255, 255, 255));
+			btnSubmit.setFont(new Font("Arial", Font.BOLD, 30));
+			btnSubmit.setBounds(552, 580, 194, 50);
+			add(btnSubmit);
+
+			lblCreate = new JLabel("Create event");
+			lblCreate.setForeground(Color.WHITE);
+			lblCreate.setFont(new Font("Arial", Font.BOLD, 78));
+			lblCreate.setBounds(451, 90, 466, 91);
+			add(lblCreate);
+
+			lblEnd = new JLabel("End");
+			lblEnd.setForeground(Color.WHITE);
+			lblEnd.setFont(new Font("Arial", Font.BOLD, 26));
+			lblEnd.setBounds(483, 349, 159, 31);
+			add(lblEnd);
+
+			lblName = new JLabel("Name");
+			lblName.setForeground(Color.WHITE);
+			lblName.setFont(new Font("Arial", Font.BOLD, 26));
+			lblName.setBounds(483, 182, 159, 31);
+			add(lblName);
+
+			lblText = new JLabel("Text");
+			lblText.setForeground(Color.WHITE);
+			lblText.setFont(new Font("Arial", Font.BOLD, 26));
+			lblText.setBounds(483, 406, 159, 31);
+			add(lblText);
+
+			textField_Name = new JTextField();
+			textField_Name.setForeground(SystemColor.controlDkShadow);
+			textField_Name.setColumns(10);
+			textField_Name.setBounds(753, 182, 120, 34);
+			add(textField_Name);
+
+			textField_Text = new JTextField();
+			textField_Text.setForeground(SystemColor.controlDkShadow);
+			textField_Text.setColumns(10);
+			textField_Text.setBounds(755, 406, 378, 66);
+			add(textField_Text);
+
+			textField_Type = new JTextField();
+			textField_Type.setForeground(SystemColor.controlDkShadow);
+			textField_Type.setColumns(10);
+			textField_Type.setBounds(755, 485, 120, 34);
+			add(textField_Type);
+
+			lblType = new JLabel("Type");
+			lblType.setForeground(Color.WHITE);
+			lblType.setFont(new Font("Arial", Font.BOLD, 26));
+			lblType.setBounds(483, 483, 159, 31);
+			add(lblType);
+			
+			JLabel lblYear = new JLabel("Year");
+			lblYear.setBounds(755, 275, 61, 16);
+			add(lblYear);
+			
+			JLabel lblNewLabel = new JLabel("Month");
+			lblNewLabel.setBounds(820, 275, 61, 16);
+			add(lblNewLabel);
+			
+			lblStartDate = new JLabel("Date");
+			lblStartDate.setBounds(885, 275, 61, 16);
+			add(lblStartDate);
+			
+			lblHour = new JLabel("Hour");
+			lblHour.setBounds(950, 275, 61, 16);
+			add(lblHour);
+			
+			lblNewLabel_1 = new JLabel("Minute");
+			lblNewLabel_1.setBounds(1015, 275, 61, 16);
+			add(lblNewLabel_1);
+			
+			lblSecond = new JLabel("Second");
+			lblSecond.setBounds(1080, 275, 61, 16);
+			add(lblSecond);
+
+		}
+		// Actionlisteners tilføjes
+		public void addActionListener(ActionListener l) {
+			btnSubmit.addActionListener(l);
+			btnBack.addActionListener(l);
+		}
+		// Her samles alt input fra textfields angående startdate til en lang string.
+//		public String startDateTimeToString(){
+//			
+//			String start = getTextFieldStartYear().getText()+"-" + getTextFieldStartMonth().getText() + "-" + 
+//					getTextFieldStartDateDay().getText() + " " + getTextFieldStartHour().getText()+":"+getTextFieldStartMinute().getText() + ":" +
+//					getTextFieldStartSecond().getText();
+//			
+//			return start;
+//		}
+//		// Her samles alt input fra textfields angående enddate til en lang string.
+//		public String endDateTimeToString(){
+//			
+//			String end = getTextFieldEndYear().getText()+"-" + getTextFieldEndMonth().getText() + "-" + 
+//					getTextFieldEndDateDay().getText() + " " + getTextFieldEndHour().getText()+":"+getTextFieldEndMinute().getText() + ":" +
+//					getTextFieldEndSecond().getText();
+//			
+//			return end;
+//		}
+		
+
+		public JTextField getTextFieldStartDateDay() {
+			return textFieldStartDateDay;
+		}
+
+		public JTextField getTextFieldEndDateDay() {
+			return textFieldEndDateDay;
+		}
+
+		public JTextField getTextFieldStartYear() {
+			return textFieldStartYear;
+		}
+
+		public JTextField getTextFieldStartMonth() {
+			return textFieldStartMonth;
+		}
+
+		public JTextField getTextFieldStartHour() {
+			return textFieldStartHour;
+		}
+
+		public JTextField getTextFieldStartMinute() {
+			return textFieldStartMinute;
+		}
+
+		public JTextField getTextFieldStartSecond() {
+			return textFieldStartSecond;
+		}
+
+		public JTextField getTextFieldEndSecond() {
+			return textFieldEndSecond;
+		}
+
+		public JTextField getTextFieldEndMinute() {
+			return textFieldEndMinute;
+		}
+
+		public JTextField getTextFieldEndHour() {
+			return textFieldEndHour;
+		}
+
+		public JTextField getTextFieldEndMonth() {
+			return textFieldEndMonth;
+		}
+
+		public JTextField getTextFieldEndYear() {
+			return textFieldEndYear;
+		}
+
+		public JTextField getTextField_Location() {
+			return textField_Location;
+		}
+
+		public JTextField getTextField_Name() {
+			return textField_Name;
+		}
+
+		public JTextField getTextField_Text() {
+			return textField_Text;
+		}
+
+		public JTextField getTextField_Type() {
+			return textField_Type;
+		}
+
+		public JButton getBtnSubmit() {
+			return btnSubmit;
+		}
+
+		public JButton getBtnBack() {
+			return btnBack;
+		
+	}
+}
---
+++ TCP/src/GUI/AddNote.java
@@ -0,0 +1,109 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class AddNote extends JPanel { 
+
+private JButton btnAddNote;
+private JTextField textFieldText;
+private JTextField textFieldCreatedBy;
+private JLabel lblCreatedBy;
+private JLabel lblNote;
+private JLabel lblAddUser;
+private JButton btnBack;
+
+private JTable table;
+private DefaultTableModel model;
+private JScrollPane scrollPane;
+
+public AddNote(){
+	
+	setPreferredSize(new Dimension(1366, 768));
+	setSize(new Dimension(1366, 768));
+	setLayout(null);
+
+	lblAddUser = new JLabel("Add note");
+	lblAddUser.setForeground(Color.WHITE);
+	lblAddUser.setFont(new Font("Arial", Font.BOLD, 78));
+	lblAddUser.setBounds(509, 107, 464, 90);
+	add(lblAddUser);
+	
+	lblCreatedBy = new JLabel("Created by:");
+	lblCreatedBy.setForeground(Color.WHITE);
+	lblCreatedBy.setFont(new Font("Arial", Font.BOLD, 26));
+	lblCreatedBy.setBounds(473, 261, 147, 39);
+	add(lblCreatedBy);
+	
+	lblNote = new JLabel("Note:");
+	lblNote.setForeground(Color.WHITE);
+	lblNote.setFont(new Font("Arial", Font.BOLD, 26));
+	lblNote.setBounds(473, 312, 105, 31);
+	add(lblNote);
+	
+	textFieldCreatedBy = new JTextField();
+	textFieldCreatedBy.setBounds(651, 267, 231, 22);
+	add(textFieldCreatedBy);
+	textFieldCreatedBy.setColumns(10);
+	
+	textFieldText = new JTextField();
+	textFieldText.setBounds(651, 302, 231, 119);
+	add(textFieldText);
+	textFieldText.setColumns(10);
+	
+	btnAddNote = new JButton("Add note");
+	btnAddNote.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		}
+	});
+	btnAddNote.setContentAreaFilled(false);
+	btnAddNote.setForeground(Color.WHITE);
+	btnAddNote.setFont(new Font("Arial", Font.BOLD, 30));
+	btnAddNote.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0, 0), new Color(255, 255, 255), new Color(0, 0, 0)), new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0, 0), new Color(255, 255, 255), new Color(0, 0, 0))));
+	btnAddNote.setBackground(Color.WHITE);
+	btnAddNote.setBounds(661, 475, 194, 50);
+	add(btnAddNote);
+	
+	btnBack = new JButton("Back");
+	btnBack.setContentAreaFilled(false);
+	btnBack.setForeground(Color.WHITE);
+	btnBack.setFont(new Font("Arial", Font.BOLD, 30));
+	btnBack.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0, 0), new Color(255, 255, 255), new Color(0, 0, 0)), new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0, 0), new Color(255, 255, 255), new Color(0, 0, 0))));
+	btnBack.setBackground(Color.WHITE);
+	btnBack.setBounds(661, 545, 194, 50);
+	add(btnBack);
+	}
+
+public void addActionListener(ActionListener l) {
+	textFieldCreatedBy.addActionListener(l);
+	textFieldText.addActionListener(l);
+	btnAddNote.addActionListener(l);
+	btnBack.addActionListener(l);
+	
+}
+public JButton getBtnAddNote() {
+	return btnAddNote;
+}
+public JTextField getTextFieldText() {
+	return textFieldText;
+}
+public JTextField getTextFieldCreatedBy() {
+	return textFieldCreatedBy;
+}
+public JButton getBtnBack() {
+	return btnBack;
+}
+}

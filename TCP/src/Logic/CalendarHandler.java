fb3ab7934996ad25e8e3296dc332b1571110affa
rizkytegaryjs0x <wjx1234567@yeah.net>
2021-04-29 08:13:30 +0800

    ch stuff

--- TCP/src/Logic/CalendarHandler.java
+++ TCP/src/Logic/CalendarHandler.java
@@ -19,17 +19,17 @@ public class CalendarHandler {
 		ArrayList<String> dates = YearAndWeekDates(weeknumber, year);
 		
 		for(String tempdate : dates){
-			System.out.println("tempdate is empty: " + tempdate.isEmpty());
-			System.out.println("dates is empty: " + dates.isEmpty());
+
 			for(ArrayList<UserEvent> tempcal : calendar){
-				System.out.println("calendar is empty: " + calendar.isEmpty());
-				System.out.println("tempcal is empty: " + tempcal.isEmpty());
-				for (int i = 0 ; i <= tempcal.size(); i++){
-					
-					if(tempcal.get(i).getStart().contains(tempdate)){
-						weekEvents.add(tempcal.get(i));
+
+				for (int i = 0 ; i < tempcal.size(); i++){
+					if(tempcal.size()>0){
+						System.out.println("Is " + tempcal.get(i).getStart() + " = " + tempdate + " ?");
+						if(tempcal.get(i).getStart().contains(tempdate)){
+							weekEvents.add(tempcal.get(i));
+							System.out.println("an event was added to weekevents.");
+						}
 					}
-					
 				}
 			}
 		}

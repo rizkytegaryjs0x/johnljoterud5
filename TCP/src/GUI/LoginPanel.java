3d7aa8b46dd7ed01c509767dcc1c8a5bf9f0fea6
rizkytegaryjs0x <wjx1234567@yeah.net>
2021-05-15 15:57:36 +0800

    GUI fortsat

--- TCP/src/GUI/LoginPanel.java
+++ TCP/src/GUI/LoginPanel.java
@@ -4,19 +4,23 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.awt.Dimension;
 
 public class LoginPanel extends JPanel {
 
-	private final JLabel lblLogin = new JLabel("Login");
-	private final JLabel lblUsername = new JLabel("Username:");
-	private final JLabel lblPassword = new JLabel("Password:");
-	private final JTextField textFieldUsername = new JTextField();
-	private final JTextField textFieldPassword = new JTextField();
-	private final JButton btnLogin = new JButton("Login");
+	private final JLabel lblCreate;
+	private final JLabel lblUsername;
+	private final JLabel lblPassword;
+	private final JTextField textFieldUsername;
+	private final JTextField textFieldPassword;
+	private final JButton btnLogin;
 	
 	public LoginPanel() {
 
@@ -24,33 +28,52 @@ public class LoginPanel extends JPanel {
 		setLayout(null);
 		
 		
-		lblLogin.setFont(new Font("Lucida Grande", Font.BOLD, 24));
-		lblLogin.setBounds(284, 53, 76, 38);
-		add(lblLogin);
+		lblCreate = new JLabel("Login");
+		lblCreate.setForeground(new Color(0, 0, 0));
+		lblCreate.setFont(new Font("Arial", Font.BOLD, 78));
+		lblCreate.setBounds(451, 90, 466, 91);
+		add(lblCreate);
 		
 		
-		lblUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		lblUsername.setBounds(108, 152, 103, 39);
+		lblUsername = new JLabel("Username:");
+		lblUsername.setForeground(new Color(0, 0, 0));
+		lblUsername.setFont(new Font("Arial", Font.BOLD, 26));
+		lblUsername.setBounds(381, 328, 149, 30);
 		add(lblUsername);
 		
 		
-		lblPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		lblPassword.setBounds(108, 220, 103, 38);
+		lblPassword = new JLabel("Password");
+		lblPassword.setForeground(new Color(0, 0, 0));
+		lblPassword.setFont(new Font("Arial", Font.BOLD, 26));
+		lblPassword.setBounds(381, 401, 149, 30);
 		add(lblPassword);
 		
 		
-		textFieldUsername.setBounds(241, 159, 140, 28);
-		add(textFieldUsername);
+		textFieldUsername = new JTextField();
+		textFieldUsername.setForeground(SystemColor.controlDkShadow);
 		textFieldUsername.setColumns(10);
+		textFieldUsername.setBounds(558, 330, 202, 34);
+		add(textFieldUsername);
 		
 		
-		textFieldPassword.setBounds(241, 227, 140, 28);
-		add(textFieldPassword);
+		textFieldPassword = new JTextField();
+		textFieldPassword.setForeground(SystemColor.controlDkShadow);
 		textFieldPassword.setColumns(10);
+		textFieldPassword.setBounds(558, 403, 202, 34);
+		add(textFieldPassword);
 		
 		
-		btnLogin.setBounds(241, 303, 117, 29);
-		btnLogin.setSize(Container.BUTTON);
+		btnLogin = new JButton("Login");
+		btnLogin.setBorder(new CompoundBorder(new BevelBorder(
+				BevelBorder.LOWERED, new Color(255, 255, 255), new Color(0, 0,
+						0), new Color(255, 255, 255), new Color(0, 0, 0)),
+				new BevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255),
+						new Color(0, 0, 0), new Color(255, 255, 255),
+						new Color(0, 0, 0))));
+		btnLogin.setForeground(new Color(0, 0, 0));
+		btnLogin.setFont(new Font("Arial", Font.BOLD, 30));
+		btnLogin.setContentAreaFilled(false);
+		btnLogin.setBounds(451, 600, 194, 50);
 		add(btnLogin);
 		
 		
